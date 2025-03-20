737f7dfa4ea693c72f924f39829ceeccd8ccd17a
Isaacosking <kyliansastre7930@gmail.com>
2025-03-20 12:04:04 +0100

    :rocket: fith commit

--- Partie 3/cesi_et_dragon.cpp
+++ Partie 3/cesi_et_dragon.cpp
@@ -1,1 +1,23 @@
 #include <iostream>
+
+class Heros {
+private:
+    char nom;
+    int vie;
+    int attaque;
+public:
+    Heros(int vie, int attaque) : vie(vie), attaque(attaque) {}
+
+    void attaquer(Heros &cible) {
+        cible.vie -= attaque;
+    }
+
+    bool estVivant() {
+        return vie > 0;
+    }
+
+    void afficher() {
+        std::cout << "Vie : " << vie << " Attaque : " << attaque << std::endl;
+    }
+
+};
