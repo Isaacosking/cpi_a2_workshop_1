649bfc144bde19a27266ccfca96722c2eef0ea61
Isaacosking <kyliansastre7930@gmail.com>
2025-03-20 10:20:14 +0100

    :rocket: second commit

--- firstclass.cpp
+++ firstclass.cpp
@@ -1,26 +1,77 @@
 #include <iostream>
-using namespace std;
+#include <cmath>
 
+// Classe Circle
 class Circle {
-    double radius;
-    public:
-        Circle(double r) {radius = r;}
-        double area() {return 3.14159*radius*radius;}
-};
-class Rectangle {
-    int width, height;
-    public:
-        Rectangle (int,int);
-        int area() {return width*height;}
+public:
+    // Attributs publics (pour rester compatible avec main, qui accède directement aux membres)
+    double x;
+    double y;
+    double r;
+    
+    // Constructeur
+    Circle(double x, double y, double r) : x(x), y(y), r(r) {}
+
+    // Méthode pour modifier la position du centre
+    void set_pos(double new_x, double new_y) {
+        x = new_x;
+        y = new_y;
+    }
+    
+    // Méthode pour calculer le périmètre
+    double perimeter() const {
+        return 2 * M_PI * r;
+    }
+    
+    // TODO: Ajout d'une méthode pour calculer l'aire du cercle
+    double area() const {
+        return M_PI * r * r;
+    }
 };
 
-Rectangle::Rectangle (int x, int y){
-    width = x;
-    height = y;
-}
+// Classe Rectangle
+class Rectangle {
+public:
+    // Attributs publics
+    double x;
+    double y;
+    double w;
+    double h;
+    
+    // Constructeur
+    Rectangle(double x, double y, double w, double h) : x(x), y(y), w(w), h(h) {}
+
+    // Méthode pour modifier la position du coin supérieur gauche
+    void set_pos(double new_x, double new_y) {
+        x = new_x;
+        y = new_y;
+    }
+    
+    // Méthode pour calculer le périmètre
+    double perimeter() const {
+        return 2 * (w + h);
+    }
+    
+    // TODO: Ajout d'une méthode pour calculer l'aire du rectangle
+    double area() const {
+        return w * h;
+    }
+};
 
 int main() {
-    Rectangle rect (3,4);
-    cout << "area : " <<rect.area();
+    // Utilisation de la classe Circle
+    Circle c(0, 0, 1);
+    std::cout << "Périmètre du cercle : " << c.perimeter() << std::endl;
+    c.set_pos(1, 1);
+    std::cout << "Coordonnées du centre : (" << c.x << ", " << c.y << ")" << std::endl;
+    std::cout << "Aire du cercle : " << c.area() << std::endl;
+
+    // Utilisation de la classe Rectangle
+    Rectangle r(0, 0, 1, 2);
+    std::cout << "Périmètre du rectangle : " << r.perimeter() << std::endl;
+    r.set_pos(-1, -1);
+    std::cout << "Coordonnées du coin supérieur gauche : (" << r.x << ", " << r.y << ")" << std::endl;
+    std::cout << "Aire du rectangle : " << r.area() << std::endl;
+    
     return 0;
-};
+}
